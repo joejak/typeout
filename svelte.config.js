489b1ff602ae138e3e807ea05106f4a2c084c9d8
Joseph Jackson <joejak93@gmail.com>
2024-11-05 00:42:06 -0600

    update svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -8,13 +8,12 @@ const config = {
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter(
 			{
-				pages: 'build',
-				assets: 'build',
-				fallback: undefined,
-				precompress: false,
-				strict: true
+				fallback: '404.html'
 			}
-		)
+		),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
 	}
 };
 
